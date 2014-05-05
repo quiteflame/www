7d378115db52d871fa13005d42e26c1794acef92
Bad <quiteflame@gmail.com>
2014-05-05 20:08:40 +0200

    test API youtube

---
+++ youtube/YoutubeClient.php
@@ -0,0 +1,15 @@
+<?php
+
+class YoutubeClient {
+	private $clinetID = '';
+
+    public function __construct ($_clientID) {
+        $this->clientID = $_clientID;
+    }
+
+    public function authenticate {
+    	
+    }
+}
+
+?>
